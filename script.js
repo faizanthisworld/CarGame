dd7d74c60eed5e4478d60494bd0e07340f633e13
faizanthisworld <149044542+faizanthisworld@users.noreply.github.com>
2023-11-09 14:32:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,13 @@ const bottomBtn = document.querySelector("#bottom")
 const leftBtn = document.querySelector("#left")
 const rightBtn = document.querySelector("#right")
 
-document.addEventListener("touchstart", () => {
-    alert("Hello world")
-})
-
-const car = new Car(canvas.width/2,canvas.height,{left : leftBtn,top:topBtn,bottom:bottomBtn,right:rightBtn});
+ 
+const btnControls= new BtnControls(leftBtn,rightBtn,topBtn,bottomBtn) 
+const car = new Car(canvas.width/2,canvas.height,btnControls);
 const obstacle = new Obstacle(10,10,50,50)
 const img  = document.querySelector("img")
  
-const btnControls= new BtnControls(leftBtn,rightBtn,topBtn,bottomBtn) 
+
 
  
 function animate() {
