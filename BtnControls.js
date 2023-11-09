abeba30c407ac4fb2f0022314096b39b5fe90325
faizanthisworld <149044542+faizanthisworld@users.noreply.github.com>
2023-11-09 11:52:20 +0530

    Create BtnControls.js

---
+++ BtnControls.js
@@ -0,0 +1,49 @@
+class BtnControls {
+    constructor(left,right,top,bottom) {
+ 
+        this.left = false
+        this.right = false
+        this.top = false
+        this.bottom = false
+        this.#addEventListener(left,right,top,bottom)
+    }
+    #addEventListener(left,right){
+         document.addEventListener("ontouchstart", (e) => {
+            switch(e.target) {
+                case left:
+                    this.left= true;
+                    break
+                case right:
+                    this.right = true;
+                    break;
+                case top:
+                    this.top = true
+                    break
+                case bottom :
+                    this.bottom = true
+                    break
+            }
+            alert(e.target)
+         })
+         document.addEventListener("ontouchend", (e) => {
+            switch(e.target) {
+                case left:
+                    this.left= false;
+                    break
+                case right:
+                    this.right = false;
+                    break;
+                case top:
+                    this.top = false
+                    break
+                case bottom :
+                    this.bottom = false
+                    break
+            }
+            alert(e.target)
+         })
+    }
+}
+ 
+  
+ 
