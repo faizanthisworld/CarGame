867c9f267ec0400f123d2972e81abae3e75d81a0
faizanthisworld <149044542+faizanthisworld@users.noreply.github.com>
2023-11-09 11:53:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 500;
  
-const car = new Car(canvas.width/2,canvas.height);
+ 
+const topBtn = document.querySelector("#top")
+const bottomBtn = document.querySelector("#bottom")
+const leftBtn = document.querySelector("#left")
+const rightBtn = document.querySelector("#right")
+
+ 
+
+const car = new Car(canvas.width/2,canvas.height,{left : leftBtn,top:topBtn,bottom:bottomBtn,right:rightBtn});
 const obstacle = new Obstacle(10,10,50,50)
 const img  = document.querySelector("img")
  
+const btnControls= new BtnControls(leftBtn,rightBtn,topBtn,bottomBtn) 
+
  
 function animate() {
-    canvas.height = window.innerHeight - 50
+    canvas.height = window.innerHeight - 200
+    canvas.width = window.innerWidth - 20
+    
     car.Update()
     car.draw(ctx,img);
     obstacle.draw(ctx)
